044fb1e5f569c909ca0423acf1ddbf2c8a5790a4
luke-d-moore <luke.d.moore1@gmail.com>
2024-05-09 15:58:41 +0100

    changes to use the generate fake realtime data
    changes to use the generate fake realtime data

--- ThreadsTasksAndRealTimeData/ThreadsTasksAndRealTimeData/Program.cs
+++ ThreadsTasksAndRealTimeData/ThreadsTasksAndRealTimeData/Program.cs
@@ -1,19 +1,26 @@
 ﻿public class Program
 {
-    static void Main(string[] args)
+    public static decimal _Data = 0.50m;
+    public static void Main(string[] args)
     {
         var mainThread = Thread.CurrentThread;
         mainThread.Name = "Main Console Thread";
 
         Console.WriteLine($"{mainThread.Name} is now starting!");
 
-        var Task1 = Task.Run(() => WriteToConsoleWithIncrement(10, 1000, "Task1"));
-        var Task2 = Task.Run(() => WriteToConsoleWithIncrement(5, 2000, "Task2"));
+        //make a task to continuously run to generate realtime data
+        var DataTask = Task.Run(() => GenerateRealTimeData(80, 250));
 
+        //run the tasks and print the real time data
+        var Task1 = Task.Run(() => WriteToConsoleWithIncrement(10, 1000, "Task1"));
+        var Task2 = Task.Run(() => WriteToConsoleWithIncrement(5, 1700, "Task2"));
+
+        //await all tasks to complete
         Task.WaitAll([Task1, Task2]);
 
+        //run the threads and print the real time data
         var thread1 = new Thread(() => WriteToConsoleWithIncrement(10, 1000, "Thread1"));
-        var thread2 = new Thread(() => WriteToConsoleWithIncrement(5, 2000, "Thread2"));
+        var thread2 = new Thread(() => WriteToConsoleWithIncrement(5, 1700, "Thread2"));
 
         thread1.Start();
         thread2.Start();
@@ -26,8 +33,18 @@
     { 
         for (int i = 0; i <= Max; i++)
         {
-            Console.WriteLine($"Writing... {i} {Name}");
+            Console.WriteLine($"Writing... {i} {Name}, current value of Data : {_Data}");
             Thread.Sleep(Milliseconds);
+        }
+    }
+
+    public static void GenerateRealTimeData(int Max, int milliseconds)
+    {
+        for(int i = 0;i <= Max; i++)
+        {
+            var rand = new Random();
+            _Data = new decimal(rand.NextDouble());
+            Thread.Sleep(milliseconds);
         }
     }
 }
